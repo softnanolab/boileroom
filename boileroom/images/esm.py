0b534ea4abd537795d78b7299112ca1a2bdeaa41
jakublala <jakublala@gmail.com>
2026-01-07 17:51:36 +0000

    chore: remove unnecessary bits from esm image

--- boileroom/images/esm.py
+++ boileroom/images/esm.py
@@ -6,10 +6,6 @@ from modal import Image
 esm_image = (
     Image.debian_slim(python_version="3.12")
     .apt_install("wget", "git")
-    .pip_install("torch>=2.5.1,<2.7.0", "torch-tensorrt", "biotite>=1.0.1")
-    .run_commands(
-        "git clone https://github.com/jakublala/my_transformers.git",
-        "cd my_transformers && pip install .",
-    )
+    .pip_install("torch>=2.5.1,<2.7.0", "torch-tensorrt", "biotite>=1.0.1", "transformers==4.49.0")
     .env({"PYTORCH_CUDA_ALLOC_CONF": "expandable_segments:True"})
 )
