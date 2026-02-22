d711553328bea5abcbba572dcc2e82b150acf015
sangioletti <stefano.steno@gmail.com>
2026-02-22 18:19:03 +0100

    Added protenix as a protein folding algorithm

--- boileroom/images/__init__.py
+++ boileroom/images/__init__.py
@@ -1,3 +1,4 @@
 from .esm import esm_image
+from .protenix import protenix_image
 
-__all__ = ["esm_image"]
+__all__ = ["esm_image", "protenix_image"]
--- boileroom/models/__init__.py
+++ boileroom/models/__init__.py
@@ -1,9 +1,13 @@
 from .esm.esmfold import ESMFold, get_esmfold
 from .esm.esm2 import ESM2, get_esm2
+from .protenix.protenix import ProtenixFold, ProtenixOutput, get_protenix
 
 __all__ = [
     "ESMFold",
     "ESM2",
+    "ProtenixFold",
+    "ProtenixOutput",
     "get_esmfold",
     "get_esm2",
+    "get_protenix",
 ]
