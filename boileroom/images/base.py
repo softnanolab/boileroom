9c284d35b7f99b37a2799b55bb78276bd4306189
Michael Krasa <michael.krasa@gmail.com>
2026-04-14 16:03:30 -0700

    Revert "[codex] fix image runtime env"

--- boileroom/images/base.py
+++ boileroom/images/base.py
@@ -2,9 +2,6 @@
 
 from modal import Image
 
-from .metadata import BASE_IMAGE_SPEC, MODAL_IMAGE_TAG_ENV, format_image_reference, get_modal_image_tag
+from .metadata import BASE_IMAGE_SPEC, format_image_reference, get_modal_image_tag
 
-_base_image_tag = get_modal_image_tag()
-base_image = Image.from_registry(format_image_reference(BASE_IMAGE_SPEC.image_name, _base_image_tag)).env(
-    {MODAL_IMAGE_TAG_ENV: _base_image_tag}
-)
+base_image = Image.from_registry(format_image_reference(BASE_IMAGE_SPEC.image_name, get_modal_image_tag()))
